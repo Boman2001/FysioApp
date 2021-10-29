6b49865cbc9fcb7938ba17d3fda7aae21d179190
MAQUES <m.bogaers@student.avanms.nl>
2021-10-29 23:08:08 +0200

    last psush b4 video

---
+++ WebApp/ViewComponents/CounterComponent.cs
@@ -0,0 +1,31 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Domain.Models;
+using Core.DomainServices.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.ViewComponents
+{
+    [ViewComponent(Name = "Counter")]
+    public class CounterComponenet : ViewComponent{
+        private readonly IRepository<Appointment> _repository;
+        private readonly IRepository<Treatment> _Trepository;
+
+        public CounterComponenet(IRepository<Appointment> repository, IRepository<Treatment> trepository)
+        {
+            _repository = repository;
+            _Trepository = trepository;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var items = _repository.Get(appointment => appointment.TreatmentDate.Date == DateTime.Today);
+            var items1 = _Trepository.Get(appointment => appointment.TreatmentDate.Date == DateTime.Today);
+            return View(items.Count() + items1.Count());
+        }
+    }
+}
