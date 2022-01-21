ef4f2d860ba17aa4bd4422aea45be59ddfcc7484
MAQUES <m.bogaers@student.avanms.nl>
2022-01-21 11:19:56 +0100

    small uml updates and added swagger to prod

--- StamApi/Startup.cs
+++ StamApi/Startup.cs
@@ -128,10 +128,9 @@ namespace StamApi
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "StamApi v1"));
             }
-
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "StamApi v1"));
             app.UseHttpsRedirection();
 
             app.UseRouting();
